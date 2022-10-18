ba633f494551e4684c485476415bfc76350e7965
todmy <tolok.d@gmail.com>
2022-10-18 12:06:30 +0300

    chore: remove old styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,6 @@ import store from 'app/store';
 
 import './App.css';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
-// primereact/resources/themes/bootstrap4-light-blue/theme.css
-// primereact/resources/themes/bootstrap4-light-purple/theme.css
-// primereact/resources/themes/bootstrap4-dark-blue/theme.css
-// primereact/resources/themes/bootstrap4-dark-purple/theme.css
-// primereact/resources/themes/md-light-indigo/theme.css
-// primereact/resources/themes/md-light-deeppurple/theme.css
-// primereact/resources/themes/md-dark-indigo/theme.css
-// primereact/resources/themes/md-dark-deeppurple/theme.css
-// primereact/resources/themes/mdc-light-indigo/theme.css
-// primereact/resources/themes/mdc-light-deeppurple/theme.css
-// primereact/resources/themes/mdc-dark-indigo/theme.css
-// primereact/resources/themes/mdc-dark-deeppurple/theme.css
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 
